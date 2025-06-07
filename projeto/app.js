196de24382bebb5c87c0beb43334eaed08ab897a
Daniel Sena <daniels_araujo@outlook.com.br>
2025-06-07 10:19:11 -0300

    03.03 - Enquanto...

--- projeto/app.js
+++ projeto/app.js
@@ -1,15 +1,20 @@
 alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute;
 
-// Se o chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso aí! Você descobriu o número secreto (${numeroSecreto})!`);
-} else {
-    if (chute < numeroSecreto) {
-        alert(`O número secreto é maior que ${chute}`);
+// Enquanto o chute for diferente igual ao número secreto
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+
+    // Se o chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso aí! Você descobriu o número secreto (${numeroSecreto})!`);
     } else {
-        alert(`O número secreto é menor que ${chute}`);
+        if (chute < numeroSecreto) {
+            alert(`O número secreto é maior que ${chute}`);
+        } else {
+            alert(`O número secreto é menor que ${chute}`);
+        }
     }
 }
